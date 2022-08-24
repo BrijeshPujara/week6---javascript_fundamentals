fdec0dff9cc470a46d24225efec6185549e898ac
Brijesh Pujara <pujara.brijesh1@gmail.com>
2022-08-24 10:23:18 +0100

    file structure

--- phase_2/01.testing/exercise/exercise.test.js
+++ phase_2/01.testing/exercise/exercise.test.js
@@ -2,20 +2,20 @@ const { add, multiply } = require("./exercise");
 
 describe("add", () => {
   test("adds 2 and 2", () => {
-    expect(add(2, 2)).toEqual(4);
+    expect(add(2, 2)).toBe(4);
   });
 
   test("adds 5 and 0", () => {
-    expect(add(5, 0)).toEqual(5);
+    expect(add(5, 0)).toBe(5);
   });
 
   test("adds 5 and 0", () => {
-    expect(add(5, 0)).toEqual(5);
+    expect(add(5, 0)).toBe(5);
   });
 });
 
 describe("multiply", () => {
   it("multiplies 2 and 2", () => {
-    expect(multiply(2, 2)).toEqual(4);
+    expect(multiply(2, 2)).toBe(4);
   });
 });
